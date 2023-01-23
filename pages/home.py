cedf500d688a0f2c15461e66517c9b9507747db1
sebacastillo <castilloclaudiosebastian@gmail.com>
2023-01-23 00:01:48 -0300

    update

--- pages/home.py
+++ pages/home.py
@@ -2,6 +2,7 @@ import dash
 from dash import dcc
 from dash import html
 import dash_bootstrap_components as dbc
+import dash_mantine_components as dmc
 from dash import dash_table
 from dash.exceptions import PreventUpdate
 import pandas as pd
@@ -19,8 +20,6 @@ from app import app
 import dash_gif_component as gif
 
 
-img_style = {"height": "250px", "width": "450px"}
-
 carrusel = [
     {"key": "1", "src": "/assets/im3_1_edited.png"},
     {"key": "2", "src": "/assets/im3_2_edited.png"},    
@@ -31,10 +30,28 @@ problemas = [
     dbc.CardHeader("Card header"),
     dbc.CardBody(
         [
-            html.H5("Card title", className="card-title"),
+            #html.H5("Card title", className="card-title"),
             html.P(
-                "This is some card content that we'll reuse",
-                className="card-text",
+                dcc.Markdown('''
+                Multiplo es un empresa dedicada a mejorar procesos
+                aplicando *Inteligencia Artificial* en tareas humanas.
+                Multiplo es un empresa dedicada a mejorar procesos
+                aplicando *Inteligencia Artificial* en tareas humanas.
+                Multiplo es un empresa dedicada a mejorar procesos
+                aplicando *Inteligencia Artificial* en tareas humanas.
+                Multiplo es un empresa dedicada a mejorar procesos
+                aplicando *Inteligencia Artificial* en tareas humanas.
+                Multiplo es un empresa dedicada a mejorar procesos
+                aplicando *Inteligencia Artificial* en tareas humanas.
+                Multiplo es un empresa dedicada a mejorar procesos
+                aplicando *Inteligencia Artificial* en tareas humanas.
+                Multiplo es un empresa dedicada a mejorar procesos
+                aplicando *Inteligencia Artificial* en tareas humanas.
+                Multiplo es un empresa dedicada a mejorar procesos
+                aplicando *Inteligencia Artificial* en tareas humanas.
+                [(ver)](/multiplo)                                                  
+                '''
+                ),       
             ),
         ]
     ),
@@ -57,15 +74,15 @@ def layout():
                                      dbc.Carousel(
                                         items= carrusel,                                       
                                         class_name="carousel-fade",
-                                        controls=False,
+                                        controls=True,
                                         indicators=True,
                                         interval=4000,
-                                        style = img_style,
+                                        variant="dark",
                                         #ride="carousel",
                                     )
                                 ]                                    
                             ),
-                            width=4,
+                            width=6,
                             ),
                             dbc.Col(html.Div(
                                 dcc.Markdown('''
@@ -86,6 +103,10 @@ def layout():
                     ],
                     align = "center", 
                     ),                
+                html.Br(),
+                html.Br(),
+                html.Br(),
+                html.Br(),
                 html.Br(),
                 html.Br(),
                 html.Br(),
@@ -160,8 +181,91 @@ def layout():
                 ),
                 html.Br(),
                 html.Br(),
+                dbc.Row(
+                    [
+                        dbc.Col(
+                            dmc.Card(
+                                children=[
+                                    dmc.CardSection(
+                                        dmc.Image(
+                                            src="https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80",
+                                            height=160,
+                                        )
+                                    ),
+                                    dmc.Group(
+                                        [
+                                            dmc.Text("Norway Fjord Adventures", weight=500),
+                                            dmc.Badge("On Sale", color="red", variant="light"),
+                                        ],
+                                        position="apart",
+                                        mt="md",
+                                        mb="xs",
+                                    ),
+                                    dmc.Text(
+                                        "With Fjord Tours you can explore more of the magical fjord landscapes with tours and activities on and around the fjords of Norway",
+                                        size="sm",
+                                        color="dimmed",
+                                    ),
+                                    dmc.Button(
+                                        "Book classic tour now",
+                                        variant="light",
+                                        color="blue",
+                                        fullWidth=True,
+                                        mt="md",
+                                        radius="md",
+                                    ),
+                                ],
+                                withBorder=True,
+                                shadow="sm",
+                                radius="md",
+                                style={"width": 350},
+                            )),
+                        dbc.Col(
+                            html.Div("One of three columns"),
+                            align = "center",
+                            style={'text-align': 'center'},),
+                        dbc.Col(
+                            html.Div("One of three columns"),
+                            align = "center",
+                            style={'text-align': 'center'},),
+                        dbc.Col(
+                            html.Div("One of three columns"),
+                            align = "center",
+                            style={'text-align': 'center'},),
+                        dbc.Col(
+                            html.Div("One of three columns"),
+                            align = "center",
+                            style={'text-align': 'center'},),
+                    ]
+                ),
                 html.Br(),
-                html.Br(),                                
+                html.Br(),
+                dbc.Row(
+                    [
+                        dbc.Col(html.Div("One of three columns"),
+                        align = "center",  
+                        style={'text-align': 'right'},                      
+                        ),
+                        dbc.Col(html.Div(
+                            dbc.Col(
+                                    gif.GifPlayer(
+                                        gif='assets/unrelatedpoints.gif',
+                                        still='assets/unrelatedpoints.gif',
+                                        autoplay = True,
+                                    )
+                                ),
+                            )),
+                        dbc.Col(html.Div("One of three columns"),
+                        align = "center",
+                        style={'text-align': 'left'},
+                        ),
+                    ],
+                    className="g-0",
+                ),
+                html.Br(),
+                html.Br(),
+                html.Br(),
+                html.Br(),                                       
                 # Final home ----
                 dbc.Row(
                     pie,
